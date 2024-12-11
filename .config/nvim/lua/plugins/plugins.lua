98d59ee13eed1d12fd1cf0b3627f2522f26db8b1
brynkii <Briankimathi1188@gmail.com>
2024-12-11 08:34:23 +0300

    Finished the neovim revamp.

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,18 +1,24 @@
 return {
-	-- Detect tabstop and shiftwidth automatically
-	"tpope/vim-sleuth",
-	"tpope/vim-surround",
-	-- Useful plugin to show you pending keybinds.
-	-- "gc" to comment visual regions/lines
-	{ "numToStr/Comment.nvim", opts = {} },
-	--used for highlighting todo and other comments
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  'tpope/vim-surround',
+  'brenoprata10/nvim-highlight-colors',
+  -- Useful plugin to show you pending keybinds.
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+  --used for highlighting todo and other comments
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
+  },
 }
